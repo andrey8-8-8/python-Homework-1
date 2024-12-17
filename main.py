e010b42774712da2fed7813d4ebcdffcb29592d6
andrey8-8-8 <andreymaster100@gmail.com>
2024-12-17 12:17:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+"""1st program"""
+
+x=9
+y=5
+r=(x**0.5)*y
+print (r)
+
+x=9
+y=5
+r=(x**0.5)*5
+print (r)
+
+'2nd program'
+result=(9.99>9.98) and (1000!=1000.1)
+print(result)
+
+"3rd program"
+result=(2*2+2)
+print(result)
+
+result=2*(2+2)
+print(result)
+
+result=(2*2+2)==2*(2+2)
+print(result)
+
+"4th program"
+string_number='123.456'
+float_number=float(string_number)
+print(float_number)
+result=(float_number)*10
+print (int(result)%10)
